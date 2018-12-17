c660aa6955214b8be1946d48d8c7da34e9aa6f32
ericmao <ericmao@tencent.com>
2018-12-17 13:16:08 +0800

    first

---
+++ EMAccountChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EMAccountChat
+//
+//  Created by Mao Yuxi on 2018/12/17.
+//  Copyright © 2018年 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EMAccountChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EMAccountChat
+//
+//  Created by Mao Yuxi on 2018/12/17.
+//  Copyright © 2018年 Tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
