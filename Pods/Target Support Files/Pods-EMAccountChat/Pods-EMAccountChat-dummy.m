8ade614627be6dc706f765b9415a8935743a09b4
ericmao <ericmao@tencent.com>
2018-12-17 14:30:26 +0800

    【AR】add pod && FMDB

---
+++ Pods/Target Support Files/Pods-EMAccountChat/Pods-EMAccountChat-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EMAccountChat : NSObject
+@end
+@implementation PodsDummy_Pods_EMAccountChat
+@end
---
+++ Pods/Target Support Files/Pods-EMAccountChatTests/Pods-EMAccountChatTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EMAccountChatTests : NSObject
+@end
+@implementation PodsDummy_Pods_EMAccountChatTests
+@end
